ea51cd7b2f5546cf40fbf70f3409908a499e2b68
Jeggam-Mahesh <jeggammahesh15720@gmail.com>
2023-06-16 15:40:56 +0530

    array of objects

---
+++ index.js
@@ -0,0 +1,60 @@
+let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+          {name: 'Baba', id: 101, marks : 23 },
+          {name: 'yaga', id: 200, marks : 45 },
+          {name: 'Wick', id: 115, marks : 75 } ];
+          //q1 .....
+        //   studentRecords.map((item)=>{
+        //     console.log(item.name.toUpperCase());
+        //   })
+
+        ///q2...
+
+        // studentRecords.filter((item)=>{
+        //     if(item.marks>50)
+        //     console.log(item);
+        //   })
+
+        //Q3...
+        // studentRecords.filter((item)=>{
+        //        if(item.marks>50&&item.id>120)
+        //        console.log(item);
+        // })
+
+        //Q4...
+
+        // let ans=studentRecords.reduce((prev,curr)=>{
+        // return prev+curr.marks;
+        //  },0)
+        //  console.log(ans);
+
+     //Q5..
+
+    //  studentRecords.filter((item)=>{
+    //     if(item.marks>50)   
+    //    console.log(item.name);
+    //      })
+    //q6///
+//    let ans=studentRecords.filter((item)=>{ 
+//        return item.marks<50;
+//          })
+//          .map((item2)=>{
+            
+//             return item2.marks+15;
+//          })
+//          .filter((item3)=>{
+//             return item3>50;
+//          })
+//          .reduce((prev,curr)=>{
+//             return prev+=curr;
+//          },0);
+//          console.log(ans);
+
+  let arrayObject=[
+    { name:"mahesh",class:"CSE",roolno:513},
+    { name:"mahesh1",class:"ECE",roolno:514},
+    { name:"mahesh32",class:"MECH",roolno:513},
+    { name:"mahesh4",class:"CIVIL",roolno:513},
+    { name:"mahesh5",class:"MINING",roolno:513},
+    { name:"mahesh6",class:"EEE",roolno:513}
+  ]   ;
+  console.log(arrayObject);      
